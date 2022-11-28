0b15b60444f15854cb2cafc81d20de669e9e4928
ecourse <smkang@codenuri.co.kr>
2022-11-28 16:48:16 +0900

    Add files via upload

--- DAY1/07_promise1.cpp
+++ DAY1/07_promise1.cpp
@@ -11,10 +11,20 @@ int add1(int a, int b)
 }
 
 // 스레드가 수행한 함수의 반환값을 꺼내려면
-// => out parameter 로 해야 합니다.
+// => out parameter (또는 전역변수)로 해야 합니다.
+// => 그런데, 이 방식은 연산의 종료를 대기할수 없습니다.
+//    스레드의 종료를 대기하게 됩니다.
+// => 해결책 : 1. promise & future
+//            2. packaged_task
+//			  3. async
+// => 결과뿐 아니라 "예외의 전달" 도 가능합니다.
 void add2(int a, int b, int& s)
 {
 	s = a + b;
+
+	// 연산은 종료 되었지만, 
+	// 마무리 작업을 해야 한다고 생각해 보세요.
+	std::this_thread::sleep_for(3s);
 }
 
 int main()
@@ -27,7 +37,8 @@ int main()
 
 	std::thread t(add2, 10, 20, std::ref(ret2) );
 
-	t.join();
+	t.join();	// 연산의 종료를 대기하는 것이 아니라!!!
+				// 스레드의 종료를 대기하는 것!
 
 	std::cout << ret2 << std::endl;
 }
