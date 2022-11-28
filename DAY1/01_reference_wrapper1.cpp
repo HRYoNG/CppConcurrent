9254c1fa022522bfe481c1759982281d4817f8ce
ecourse <smkang@codenuri.co.kr>
2022-11-28 09:29:26 +0900

    Add files via upload

---
+++ DAY1/01_reference_wrapper1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int v1 = 10, v2 = 20;
+
+	// C++ 참조 : 이동 불가능한 참조, 의미론적으로 상수 입니다.
+	//			  "r1 = r2" 는 가리키던 변수의 값을 대입합니다.
+	// int& r1 = v1;
+	// int& r2 = v2;
+
+	// 이동 가능한 참조 : 대입 연산시 참조가 이동합니다.(C++11)
+	std::reference_wrapper<int> r1(v1);
+	std::reference_wrapper<int> r2(v2);
+
+	r1 = r2;
+
+	// 아래 코드 결과 예측해 보세요		// C++참조
+	std::cout << v1 << std::endl;	// 20
+	std::cout << v2 << std::endl;	// 20
+	std::cout << r1 << std::endl;	// 20
+	std::cout << r2 << std::endl;	// 20
+}
