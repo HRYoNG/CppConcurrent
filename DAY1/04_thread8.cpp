969ffdba2cdb4752aebec6d3876d7c133addb478
ecourse <smkang@codenuri.co.kr>
2022-11-28 14:11:41 +0900

    Add files via upload

--- DAY1/04_thread8.cpp
+++ DAY1/04_thread8.cpp
@@ -19,12 +19,22 @@ int main()
 
     for (int i = 0; i < 10; i++)
     {
-        // 방법 1.
-        std::thread t(&do_work, 3);
-        v.push_back(t);
+        // 방법 1. thread 객체 생성후 move 로 넣기
+//      std::thread t(&do_work, 3);
+//      v.push_back(t); // error. 복사 생성자 사용하는데 copy 안됨.
+//      v.push_back( std::move(t) ); // ok
 
+        // 방법 2. 임시객체로 넣기
+//      v.push_back( std::thread(&do_work, 3) );  // ok
 
+        // 방법 3. emplace_back 사용 - 가장 좋은 방법
+        // => std::thread 의 객체를 만들어서 보내지 말고
+        // => std::thread 를 만들기 위한 인자를 전달
+        v.emplace_back( &do_work, 3 );
+                // => 내부적으로 std::thread(&do_work, 3)으로만들어서
+                //    버퍼에 보관
     }
+    std::for_each(v.begin(), v.end(), [](auto& t) { t.join(); });
 
 
 }
