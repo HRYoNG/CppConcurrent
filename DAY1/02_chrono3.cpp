5ad735588569e92b8dab64dd7b5206b704492c5b
ecourse <smkang@codenuri.co.kr>
2022-11-28 10:40:54 +0900

    Add files via upload

--- DAY1/02_chrono3.cpp
+++ DAY1/02_chrono3.cpp
@@ -13,4 +13,12 @@ int main()
 
 	// 방법 2. "user define literals" 문법으로 만들어진 접미사 사용
 	auto s2 = 30s; // std::chrono::seconds operator""s(30) 이라는 함수가 호출됩니다.
+
+	// 함수에 시간을 전달하는 방법
+	foo(30); // error. seconds 타입이 아닙니다.
+	foo(std::chrono::seconds(30)); // 방법 1. 
+	foo(30s);  // 방법 2.
+
+	foo(20ns); // 캐스팅 없이 전달 가능
+
 }
