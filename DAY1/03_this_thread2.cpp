ac2bae2028b1e3933746a9f513fe3fbe1444ebf8
ecourse <smkang@codenuri.co.kr>
2022-11-28 11:28:26 +0900

    Add files via upload

--- DAY1/03_this_thread2.cpp
+++ DAY1/03_this_thread2.cpp
@@ -3,25 +3,6 @@
 #include <chrono>
 using namespace std::literals;
 
-int main()
-{
-    // 2. sleep_for : 특정 시간 동안 현재 스레드 재우기
-//  std::this_thread::sleep_for(3); // error
-    std::this_thread::sleep_for(std::chrono::seconds(3)); 
-    std::this_thread::sleep_for(3s);
-
-
-    // 3. sleep_until : 특정 시간 "까지" 재우기
-    std::this_thread::sleep_until(
-        std::chrono::system_clock::now() + 200ms);
-}
-
-
-
-
-
-
-/*
 
 time_t toUTC(std::tm& timeinfo)
 {
@@ -46,4 +27,27 @@ std::chrono::system_clock::time_point createDateTime(int year, int month, int da
     time_t tt = toUTC(timeinfo);
     return std::chrono::system_clock::from_time_t(tt);
 }
-*/
+
+int main()
+{
+    // 2. sleep_for : 특정 시간 동안 현재 스레드 재우기
+//  std::this_thread::sleep_for(3); // error
+    std::this_thread::sleep_for(std::chrono::seconds(3)); 
+    std::this_thread::sleep_for(3s);
+
+
+    // 3. sleep_until : 특정 시간 "까지" 재우기
+    std::this_thread::sleep_until(
+        std::chrono::system_clock::now() + 200ms);
+
+    // 특정 날짜 까지 재우기
+    auto t1 = createDateTime(2022, 11, 28, 11, 50, 0);
+
+    std::this_thread::sleep_until(t1);
+
+    // 4. yield : 현재 스레드 실행을 포기하고 다른 스레드를 실행하라는 명령
+    std::this_thread::yield();
+}
+
+
+
