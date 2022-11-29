daab7ded0b26a353d8119543b839d964414ce701
ecourse <smkang@codenuri.co.kr>
2022-11-29 09:28:22 +0900

    Add files via upload

--- DAY2/00_promise3-1.cpp
+++ DAY2/00_promise3-1.cpp
@@ -4,6 +4,11 @@
 #include <chrono>
 using namespace std::literals;
 
+// promise
+// => 생성된 스레드가 "값" 과 "예외" 를 부모 스레드에게 전달할수 있는 도구
+// => future 객체를 통해서 "값"과 "예외"를 대기 할수 있다.
+
+
 void divide(std::promise<int>& p, int a, int b)
 {
     try
@@ -28,7 +33,6 @@ int main()
     std::future<int>  ft = pm.get_future();
 
     std::thread t(divide, std::ref(pm), 10, 0 );
-
 
     try
     {
