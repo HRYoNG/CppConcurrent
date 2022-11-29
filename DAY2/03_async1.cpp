ca9e4491f7bb7fc65bc35545e0b32394bb264929
ecourse <smkang@codenuri.co.kr>
2022-11-29 11:03:59 +0900

    Add files via upload

--- DAY2/03_async1.cpp
+++ DAY2/03_async1.cpp
@@ -8,16 +8,17 @@ int add(int a, int b)
 {
 	std::cout << "add" << std::endl;
 
+	std::this_thread::sleep_for(2s);
 	return a + b;
 }
 
 int main()
 {
-	std::packaged_task<int(int, int)> task(add);
-
-	std::future<int> ft = task.get_future();
-
-	std::thread t(std::move(task), 10, 20);
+	// std::async : 특정 함수를 비동기(스레드)로 실행하고
+	//				결과를 얻을수 있는 future를 반환
+	std::future<int> ft = std::async(add, 10, 20);
+			
+	std::cout << "main 계속 실행" << std::endl;
 
 	int ret = ft.get();
 
