c05ab3aa7b5aaa564d8b56e9d58d7babba3c5132
ecourse <smkang@codenuri.co.kr>
2022-11-29 11:31:38 +0900

    Add files via upload

--- DAY2/03_async2.cpp
+++ DAY2/03_async2.cpp
@@ -15,11 +15,29 @@ int main()
 {
 //  std::future<int> ft = std::async( add, 10, 20);
 
+    // 핵심 1. launch 옵션
     // std::launch::async : 비동기로 실행해 달라. 새로운 스레드생성
     // std::launch::deferred : 지연된 실행. ft.get() 할때 실행됨
     //                          주스레드가 직접 호출
-    // std::future<int> ft = std::async(std::launch::async,  add, 10, 20);
-    std::future<int> ft = std::async(std::launch::deferred, add, 10, 20);
+    // std::future<int> ft = std::async(std::launch::async, add, 10, 20);
+    // std::future<int> ft = std::async(std::launch::deferred, add, 10, 20);
+
+    
+
+
+    // 핵심 2. launch 옵션은 OR(|) 가능합니다.
+    // => 스레드가 지원되는 시스템에서는 "스레드"로 수행
+    // => 스레드가 지원되지 않은 시스템에서는 "지연된실행"으로 수행
+    //std::future<int> ft = 
+    //    std::async(std::launch::async | std::launch::deferred, add, 10, 20);
+
+
+    // 핵심 3. launch 옵션을 생략하면
+    // => "std::launch::async | std::launch::deferred" 사용
+
+    std::future<int> ft = std::async( add, 10, 20);
+
+
 
 
 
