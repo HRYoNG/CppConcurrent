19ba4e168058a3944bbd2e907579b0d6972b5cb7
ecourse <smkang@codenuri.co.kr>
2022-11-30 09:19:22 +0900

    Add files via upload

---
+++ DAY3/01_condition2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+using namespace std::literals;
+
+
+std::mutex m;
+int shared_data = -1; 
+
+// 1. condition_variable 타입의 전역변수를 생성합니다.
+std::condition_variable cv;
+
+
+void consumer()
+{
+    // 2. 신호를 대기하는 스레드는  unique_lock 으로 뮤텍스 획득후 
+    //    신호를 대기 합니다.
+    std::unique_lock<std::mutex> ul(m);
+
+    cv.wait(ul);
+
+    std::cout << "consume : " << shared_data << std::endl;
+}
+void producer()
+{
+    std::this_thread::sleep_for(10ms);
+    {
+        std::lock_guard<std::mutex> lg(m);
+        shared_data = 100;
+        std::cout << "produce : " << shared_data << std::endl;
+    }
+    // 3. 신호를 주는 스레드는 unique_lock 을 사용하지 않아도 됩니다.
+    // 공유자원이 준비 되면 신호를 주면 됩니다.
+    // 주의사항은 "신호를 주기전에 unlock" 하고 신호를 주세요
+    cv.notify_one();
+}
+
+int main()
+{
+    std::thread t1(producer);
+    std::thread t2(consumer);
+    t1.join();
+    t2.join();
+}
