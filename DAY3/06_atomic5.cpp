1c1a83b4bec4bda710fee82dd0090657cbec3bb7
ecourse <smkang@codenuri.co.kr>
2022-11-30 13:28:33 +0900

    Add files via upload

--- DAY3/06_atomic5.cpp
+++ DAY3/06_atomic5.cpp
@@ -7,15 +7,15 @@ using namespace std::literals;
 void init()
 {
     std::cout << "start init" << std::endl;
-    
     std::this_thread::sleep_for(5s);
-
     std::cout << "end init" << std::endl;
 }
 
+std::once_flag flag;
+
 void foo()
 {
-    init();
+    std::call_once(flag, init);
 
     std::cout << std::this_thread::get_id() << "finish foo" << std::endl;
 }
