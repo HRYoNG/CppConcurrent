ab6c16cd9bd8d8fa941828c7e4cd2963f3f95870
ecourse <smkang@codenuri.co.kr>
2022-11-30 10:10:25 +0900

    Add files via upload

--- DAY3/02_producer_consumer2.cpp
+++ DAY3/02_producer_consumer2.cpp
@@ -10,6 +10,11 @@ std::queue<int> Q;
 // 접근 동기화를 위한 mutex
 std::mutex m;
 
+
+// 아래 코드는 Q 의 동시접근은 해결했지만
+// 문제 점은 Q가 비어 있을때입니다.
+// => Q가 비어 있다면 "소비자는 대기" 해야 합니다.
+
 void producer()
 {
 	while (true)
