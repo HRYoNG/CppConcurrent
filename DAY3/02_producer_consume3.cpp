e91ce29d39861f5bb65efe28507290b3157a2ecf
ecourse <smkang@codenuri.co.kr>
2022-11-30 10:18:54 +0900

    Add files via upload

--- DAY3/02_producer_consume3.cpp
+++ DAY3/02_producer_consume3.cpp
@@ -5,6 +5,10 @@
 #include <mutex>
 #include <condition_variable>
 
+// 아래 코드는 "Q가 가득 찰때" 비정상 종료 될수있습니다.
+// => 해결해 보세요.. !!
+
+
 std::queue<int> Q;
 
 // 접근 동기화를 위한 mutex
@@ -41,7 +45,7 @@ void consume()
 		// Q 가 비어있을때만 대기 하면 됩니다.
 		// cv.wait(ul, !Q.empty()); // error. 2번째 인자로는 "bool"이 아닌
 			   					    // 함수가 전달되어야 합니다.
-
+		 
 		cv.wait(ul, [] {return !Q.empty(); }); 
 								// => 이 예제는 Q.empty()로 조사하므로
 								// => "data_ready" 같은 flag가 없어도 됩니다.
