d31c75b6620d1e7e83735a30b07327aa8eae0755
ecourse <smkang@codenuri.co.kr>
2022-11-30 13:19:37 +0900

    Add files via upload

---
+++ DAY3/06_atomic4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <atomic>
+
+struct Point
+{
+    int x, y;
+
+    Point() = default;
+
+    Point(const Point& other ) : x(other.x), y(other.y) {}
+
+    //Point(const Point&) = default;
+};
+
+std::atomic<Point> pt;
+
+int main()
+{
+    Point ret = pt.load();
+}
+
