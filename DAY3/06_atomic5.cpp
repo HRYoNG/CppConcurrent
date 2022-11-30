1ed08abaec4f08ae9179c60dc0eb76de894f539e
ecourse <smkang@codenuri.co.kr>
2022-11-30 13:32:53 +0900

    Add files via upload

--- DAY3/06_atomic5.cpp
+++ DAY3/06_atomic5.cpp
@@ -11,14 +11,42 @@ void init()
     std::cout << "end init" << std::endl;
 }
 
+/*
 std::once_flag flag;
 
 void foo()
 {
+    // 먼저 도착한 스레드가 init() 호출
+    // 나중에 도착한 스레드는 init() 종료 까지 대기 
     std::call_once(flag, init);
 
     std::cout << std::this_thread::get_id() << "finish foo" << std::endl;
 }
+*/
+// blocking 되지 않은 call_once 를 만들어 봅시다.
+
+template<typename F>
+void call_once_nonblock(std::atomic<bool>& flag, F func)
+{
+    bool b = false;
+
+    if (flag.compare_exchange_weak(b, true))
+    {
+        func();
+    }
+}
+
+std::atomic<bool> flag;
+
+void foo()
+{
+    std::call_once_nonblock(flag, init);
+
+    std::cout << std::this_thread::get_id() << "finish foo" << std::endl;
+}
+
+
+
 
 int main()
 {
