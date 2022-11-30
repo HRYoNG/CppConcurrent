87ffcf6d1d7f964c69636a95843ed61c51de369c
ecourse <smkang@codenuri.co.kr>
2022-11-30 13:23:58 +0900

    Add files via upload

--- DAY3/06_atomic4.cpp
+++ DAY3/06_atomic4.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <atomic>
+// trivial 복사 생성자
+// => 사용자가 만들지 않고, 컴파일러가 만들어 주는 디폴트 버전을 사용하고
+// => 가상함수가 없는 경우..
+// => memcpy 로 복사 하는 것과 동일한 경우
+
+// atomic<T> : T의 복사(이동)생성자가 trivial 해야 합니다.
 
 struct Point
 {
@@ -7,15 +13,18 @@ struct Point
 
     Point() = default;
 
-    Point(const Point& other ) : x(other.x), y(other.y) {}
+//  Point(const Point& other ) : x(other.x), y(other.y) {}
 
     //Point(const Point&) = default;
 };
 
 std::atomic<Point> pt;
 
+
 int main()
 {
-    Point ret = pt.load();
+    Point ret = pt.load(); // Point 타입에 복사 생성자가 있다면
+                           // 단순히 메모리 복사가 아닌
+                            // "복사 생성자를 호출해서 복사 해야 합니다."
 }
 
