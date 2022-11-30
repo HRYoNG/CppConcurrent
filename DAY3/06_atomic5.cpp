954840336f7f95d682e147e506e181b7ad428306
ecourse <smkang@codenuri.co.kr>
2022-11-30 13:41:10 +0900

    Add files via upload

--- DAY3/06_atomic5.cpp
+++ DAY3/06_atomic5.cpp
@@ -4,7 +4,7 @@
 #include <chrono>
 using namespace std::literals;
 
-void init()
+void init(int n, double d)
 {
     std::cout << "start init" << std::endl;
     std::this_thread::sleep_for(5s);
@@ -25,23 +25,40 @@ void foo()
 */
 // blocking 되지 않은 call_once 를 만들어 봅시다.
 
-template<typename F>
-void call_once_nonblock(std::atomic<bool>& flag, F func)
+
+template<typename F, typename ... ARGS>
+void call_once_nonblock(std::atomic<bool>& flag, F func,
+                        ARGS&& ... args )
 {
     bool b = false;
 
+    // flag.compare_exchange_weak(b, true) 의 의미
+    // => 아래 코드는 lock-free로 실행!
+    /*
+    if (flag == b)
+    {
+        flag = !b;
+        return true;
+    }
+    else
+    {
+        b = !b;
+        return false;
+    }
+    */
+
     if (flag.compare_exchange_weak(b, true))
     {
-        func();
+        func( std::forward<ARGS>(args)... );
     }
 }
 
-std::atomic<bool> flag;
+std::atomic<bool> flag; // false, 즉, 아직 init 함수 호출되지 않았다는것 
 
 void foo()
 {
 //     std::call_once(falg, init); C++ 표준, 늦게 도착하면 block
-    call_once_nonblock(flag, init);
+    call_once_nonblock(flag, init, 3, 5.5 );
 
     std::cout << std::this_thread::get_id() << "finish foo " << std::endl;
 }
